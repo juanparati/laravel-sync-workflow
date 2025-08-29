c6d290467efc9db2c57a0d0ad9f1dce3b88d7c7d
Juan Lago <juan@leadsupply.com>
2025-08-29 11:37:24 +0200

    Add `SyncWorkflowReplayCommand` for workflow replay and enhance `SyncWorkflowViewCommand` with detailed state display.

---
+++ src/Console/Commands/SyncWorkflowReplayCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Juanparati\SyncWorkflow\Console\Commands;
+
+use Illuminate\Console\Command;
+use Juanparati\SyncWorkflow\Exceptions\SyncWorkflowControlledException;
+use Juanparati\SyncWorkflow\Models\SyncWorkflowState;
+use Juanparati\SyncWorkflow\SyncExecutor;
+use Juanparati\SyncWorkflow\Test\Fixtures\Workflows\GenericActivities\ControlledExceptionActivity;
+
+class SyncWorkflowReplayCommand extends Command
+{
+    protected $signature = 'sync-workflow:replay {id}
+        {--force : Force replay workflows even if they were successful executed}
+    ';
+
+    protected $description = 'Replay workflow';
+
+    public function handle(): int
+    {
+        if (!($state = SyncWorkflowState::find($this->argument('id')))) {
+            $this->error('Workflow state not found.');
+            return -1;
+        }
+
+        if (!$this->option('force') && $state->was_successful) {
+            $this->error('Workflow was successful executed.');
+            return -1;
+        }
+
+        $executor = SyncExecutor::make($this->argument('id'))->load($state->instance);
+
+        $this->info("Loaded workflow {$executor->getId()}");
+        $this->info('Running workflow...');
+
+        try {
+            $executor->run();
+        } catch (SyncWorkflowControlledException $e) {
+            $this->warn('Controlled exception:' . $e->getMessage());
+            return 0;
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+            return -1;
+        }
+
+        $this->output->success('Workflow finished successfully.');
+
+        return 0;
+    }
+}
--- src/Console/Commands/SyncWorkflowViewCommand.php
+++ src/Console/Commands/SyncWorkflowViewCommand.php
@@ -19,12 +19,25 @@ class SyncWorkflowViewCommand extends Command
             return -1;
         }
 
+        $this->output->title('Workflow state: ' . $state->id);
+
         $state = $state->toArray();
 
-        $this->table(
-            array_keys($state),
-            array_values($state)
-        );
+        foreach ($state as $key => $value) {
+            $this->info(str($key)->ucfirst()->append(':'));
+
+            if (is_null($value)) {
+                $value = 'NULL';
+            }
+
+            if (is_array($value)) {
+                $value = json_encode($value, JSON_PRETTY_PRINT);
+            }
+
+            $this->line($value);
+            $this->newLine();
+        }
+
 
         return 0;
     }
