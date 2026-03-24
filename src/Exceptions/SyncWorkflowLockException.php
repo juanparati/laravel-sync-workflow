88bc064eee3e59fdb0fb7c2f275755ed77e08e07
Juan Lago <juan@leadsupply.com>
2026-03-24 09:39:04 +0100

    Add `SyncWorkflowLockException` for controlled workflow exception handling.

---
+++ src/Exceptions/SyncWorkflowLockException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Juanparati\SyncWorkflow\Exceptions;
+
+/**
+ * Controlled exception for workflow.
+ */
+class SyncWorkflowLockException extends \Exception
+{
+}
